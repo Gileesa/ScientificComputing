27dd9938dfb60fef89e13346cd26351deb435c20
SooriyaaK <s.karunaharan@student.vu.nl>
2026-02-09 16:59:46 +0100

    deleted the function

--- assignment_1_1.py
+++ assignment_1_1.py
@@ -41,30 +41,6 @@ def propagate_wave(u, N, Nt, r):
 u = initial_wave_profile(u, N, r)
 u = propagate_wave(u, N, Nt, r)
 
-# def initialise_wave(matrix):
-#     Nx = matrix.shape[1]
-#     x = np.linspace(0, L, N)
-#     matrix[0, :] = np.sin(2 * np.pi * x)
-#     # print(matrix[0])
-#     return matrix
-
-# A = initialise_wave(A)
-
-# def propagate_wave(A):
-#     for j in range(1,t-1):
-#         print(' j', j)
-#         print(A[j])
-#         for i in range(0,N-1):
-#             print(i)
-#             print(A[j,i])
-#             new_u = c**2 * ((dt**2)/(l**2)) * (A[i+1,j] + A[i-1,j] - 2*A[i,j]) - A[i,j-1] + 2*A[i,j]
-#             A[i,j+1] = new_u
-#         A[0, j+1] = 0
-#         A[N-1, j+1] = 0
-#     print(A)
-
-# propagate_wave(A)
-
 def animate_wave(u_matrix):
     '''
     Function that animates a wave in 2D
@@ -94,7 +70,6 @@ def animate_wave(u_matrix):
         blit=True
     )
     plt.show()
-
 
 # plot initial, mid, final
 plt.plot(x, u[0, :], label="t = 0")
