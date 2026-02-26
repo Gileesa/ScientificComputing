d8c19634c200f3f7745edbe2bad26952da1eed30
Gileesa McCormack <gileesa@icloud.com>
2026-02-26 17:40:22 +0100

    Animation created Simulation is now wroking and has animation

--- Assignment2/assignment_2_3.py
+++ Assignment2/assignment_2_3.py
@@ -3,23 +3,43 @@
 #
 
 import numpy as np
+import matplotlib.pylab as plt
+from matplotlib.animation import FuncAnimation
 
-x_max = 1 # max x
-y_max = 1 # max y
-N = 50 # number of x and y steps
+N = 200 # number of x and y steps
 
-dx = x_max / N # x-step of simulation 1/50 -> 0.02
+dx = 1 # x-step of simulation
 dy = dx # y-step of simulation
-t_max = 1 # max time of simulation
-D = 1 # diffusion coeff.
+t_max = 10000 # max time of simulation
 
-dt = 0.25 * dx**2 / D # time step of simulation, set to be stable (D*dt/dx^2 <= 0.25)
+dt = 1 # time step of simulation, set to be stable (D*dt/dx^2 <= 0.25)
 N_t = int(t_max/dt) # number of time steps 1 / (0.25 * 0.004 /1) = 1000 time steps
 
-f = 0.6
+f = 0.035
+k = 0.060
+r = 10
+c_v_init = 0.25
+Dv = 0.08
+Du = 0.16
 
 
 def update_v_one_step(umatrix, vmatrix, f, D, dt, dx, k):
+    ''' 
+    Function that updates concentration matrix for substance V for 1 time step.
+    Contains diffusion, reaction and decay terms according to Gray-Scott model.
+
+    Params:
+    - umatrix [np.ndarray]: the concentration matrix for substance U
+    - vmatrix [np.ndarray]: the concentration matrix for substance V
+    - f [float]: constant involved in decay of substance V
+    - D [float]: diffusion constant for substance V
+    - dt [float]: time step
+    - dx [float]: spatial step in both x and y direction
+    - k [float]: constant involved in decay of substance V
+
+    Returns:
+    - next_matrix [np.ndarray]: updated concentration matrix for substance V
+    '''
 
     # set alpha constant
     alpha = D * dt / dx**2
@@ -38,8 +58,8 @@ def update_v_one_step(umatrix, vmatrix, f, D, dt, dx, k):
         4 * vmatrix[1:-1, 1:-1]
     )
 
-    reaction_term = umatrix[1:-1, 1:-1] * vmatrix[1:-1,1:-1]
-    decay_term = (f + k) * vmatrix
+    reaction_term = umatrix[1:-1, 1:-1] * vmatrix[1:-1,1:-1]**2
+    decay_term = (f + k) * vmatrix[1:-1,1:-1]
 
     next_matrix[1:-1, 1:-1] = (
         vmatrix[1:-1, 1:-1]
@@ -58,6 +78,22 @@ def update_v_one_step(umatrix, vmatrix, f, D, dt, dx, k):
     return next_matrix
 
 def update_u_one_step(umatrix, vmatrix, f, D, dt, dx):
+
+    ''' 
+    Function that updates concentration matrix for substance U for 1 time step.
+    Contains diffusion, reaction and replenish terms according to Gray-Scott model.
+
+    Params:
+    - umatrix [np.ndarray]: the concentration matrix for substance U
+    - vmatrix [np.ndarray]: the concentration matrix for substance V
+    - f [float]: constant involved in replenishing of substance U
+    - D [float]: diffusion constant for substance U
+    - dt [float]: time step
+    - dx [float]: spatial step in both x and y direction
+
+    Returns:
+    - next_matrix [np.ndarray]: updated concentration matrix for substance U
+    '''
 
     # set alpha constant
     alpha = D * dt / dx**2
@@ -94,3 +130,80 @@ def update_u_one_step(umatrix, vmatrix, f, D, dt, dx):
     next_matrix[:, -1] = umatrix[:, -2]
 
     return next_matrix
+
+def init_vmatrix(N, r, c):
+    vmatrix = np.zeros((N,N))
+
+    # create square
+    start_y = N//2 - r//2
+    end_y   = start_y + r
+    start_x = N//2 - r//2
+    end_x   = start_x + r
+
+    vmatrix[start_y:end_y, start_x:end_x] = c
+    return vmatrix
+
+
+def run_gray_scott(N, r, c_v_init, f,Dv,Du,dt,dx,k):
+    umatrix = np.full((N,N), 0.5)
+    vmatrix = init_vmatrix(N, r, c_v_init)
+
+    u_matrices = [umatrix]
+    v_matrices = [vmatrix]
+
+    for _ in range(N_t):
+        umatrix = update_u_one_step(umatrix, vmatrix, f,Du,dt,dx)
+        u_matrices.append(umatrix)
+        vmatrix = update_v_one_step(umatrix, vmatrix, f,Dv,dt,dx,k)
+        v_matrices.append(vmatrix)
+
+    return u_matrices, v_matrices
+
+
+def create_animation(matrices_over_time,  title):
+    '''
+    Creates animation for concentration over time.
+    
+    Params:
+    - matrices_over_time: list of matrices, each matrix representing 1 timestep.
+    '''
+    fig, ax = plt.subplots()
+
+    ax.set_title(title)
+
+    img = ax.imshow(
+        matrices_over_time[0],
+        vmin=0,
+        vmax=1,
+        origin='lower',
+        extent=[0, 1, 0, 1]
+    )
+
+    fig.colorbar(img, ax=ax)
+
+    def update(frame):
+        img.set_data(matrices_over_time[frame])
+        return img,
+
+    anim = FuncAnimation(
+        fig,
+        update,
+        frames=len(matrices_over_time),
+        interval=10,
+        blit=True
+    )
+
+    # anim.save("Figures/2.3/grayscott1.gif", fps=20)
+
+    plt.show()
+    plt.close(fig)
+
+
+Du = 0.16
+Dv = 0.08
+f  = 0.026
+k  = 0.051
+
+u_matrices, v_matrices = run_gray_scott(N,r,c_v_init,f,Dv,Du,dt,dx,k)
+create_animation(v_matrices, "V over time")
+# create_animation(u_matrices, "U over time")
