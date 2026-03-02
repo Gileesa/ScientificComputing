6d41f9259e4af4652e92d41b81a1b02f66116ac5
Gileesa McCormack <gileesa@icloud.com>
2026-03-02 09:00:51 +0100

    Deleted Figures hope this works

--- Assignment2/assignment_2_3.py
+++ Assignment2/assignment_2_3.py
@@ -329,7 +329,7 @@ plot_l2_norm_over_time(total_norm, dt, title="Total L² Norm Over Time (Coral)")
 # Spirals
 Du, Dv, f, k = 0.12, 0.08, 0.020, 0.050
 u_matrices, v_matrices, unorms, vnorms = run_gray_scott(N,r,c_v_init,f,Dv,Du,dt,dx,k)
-# create_animation(v_matrices, "V over time")
+create_animation(v_matrices, "V over time")
 plot_last_frame(v_matrices[-1], title=f"Concentration of V at t=10000 for ICs: \n Du={Du}, Dv={Dv}, f={f}, k={k}")
 plot_l2_norm_over_time(unorms, dt, title="L² Norm of U Over Time (Spiral)")
 plot_l2_norm_over_time(vnorms, dt, title="L² Norm of V Over Time (Spiral)")
